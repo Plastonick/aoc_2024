259bdaecc41f57a875622830b530cca1c3dc1939
David Pugh <plastonick@gmail.com>
2024-12-08 18:31:14 +0000

    day 7

--- README.md
+++ README.md
@@ -19,19 +19,20 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 <!--- advent_readme_stars table --->
 
 <!--- benchmarking table --->
-
 ## Benchmarks
 
-|           Day            |  Part 1   |  Part 2   |
-|:------------------------:|:---------:|:---------:|
-| [Day 1](./src/bin/01.rs) | `62.7µs`  | `75.8µs`  |
+| Day | Part 1 | Part 2 |
+| :---: | :---: | :---:  |
+| [Day 1](./src/bin/01.rs) | `62.7µs` | `75.8µs` |
 | [Day 2](./src/bin/02.rs) | `136.1µs` | `276.9µs` |
-| [Day 3](./src/bin/03.rs) | `42.2ms`  | `41.6ms`  |
-| [Day 4](./src/bin/04.rs) |  `2.6ms`  | `508.6µs` |
+| [Day 3](./src/bin/03.rs) | `42.2ms` | `41.6ms` |
+| [Day 4](./src/bin/04.rs) | `2.6ms` | `508.6µs` |
 | [Day 5](./src/bin/05.rs) | `148.1µs` | `142.6µs` |
-| [Day 6](./src/bin/06.rs) | `621.1µs` |  `1.2s`   |
+| [Day 6](./src/bin/06.rs) | `621.1µs` | `1.2s` |
+| [Day 7](./src/bin/07.rs) | `12.8ms` | `487.4ms` |
+| [Day 8](./src/bin/08.rs) | `34.5µs` | `102.9µs` |
 
-**Total: 1288.37ms**
+**Total: 1788.71ms**
 <!--- benchmarking table --->
 
 ---
---
+++ src/bin/07.rs
@@ -0,0 +1,98 @@
+use std::cmp::PartialEq;
+
+advent_of_code::solution!(7);
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let operations = vec![Operation::Add, Operation::Mul];
+
+    find_test_sum(input, &operations)
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let operations = vec![Operation::Add, Operation::Mul, Operation::Concat];
+
+    find_test_sum(input, &operations)
+}
+
+fn find_test_sum(input: &str, operations: &Vec<Operation>) -> Option<usize> {
+    let equations = parse(input);
+    let valid = equations
+        .into_iter()
+        .filter_map(|(test, numbers)| {
+            if is_valid(&test, &numbers, &operations) {
+                Some(test)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<usize>>();
+
+    Some(valid.iter().sum())
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum Operation {
+    Add,
+    Mul,
+    Concat,
+}
+
+fn is_valid(test: &usize, numbers: &Vec<usize>, operations: &Vec<Operation>) -> bool {
+    if numbers.len() == 1 {
+        return numbers.get(0) == Some(test);
+    }
+
+    for operation in operations {
+        let new_first = match operation {
+            Operation::Add => numbers[0] + numbers[1],
+            Operation::Mul => numbers[0] * numbers[1],
+            Operation::Concat => format!("{}{}", numbers[0], numbers[1])
+                .parse::<usize>()
+                .unwrap(),
+        };
+        let mut new_numbers = vec![new_first];
+        for number in numbers.iter().skip(2) {
+            new_numbers.push(*number)
+        }
+
+        if is_valid(&test, &new_numbers, &operations) {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn parse(input: &str) -> Vec<(usize, Vec<usize>)> {
+    input
+        .lines()
+        .map(|l| {
+            let (test, partial) = l.split_once(": ").unwrap();
+
+            (
+                test.parse::<usize>().unwrap(),
+                partial
+                    .split(' ')
+                    .map(|num| num.parse::<usize>().unwrap())
+                    .collect::<Vec<usize>>(),
+            )
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(3749));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(11387));
+    }
+}
