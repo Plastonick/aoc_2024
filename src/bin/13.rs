a344ac5cb59bf569503855aac201de2ef0f88e05
David Pugh <plastonick@gmail.com>
2024-12-13 09:38:05 +0000

    solve 13 _mathematically_

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,45 +1,55 @@
 use euclid::{point2, Point2D};
 use regex::Regex;
-use std::ops::{Mul, Sub};
+use std::ops::Mul;
 
 advent_of_code::solution!(13);
 
 #[derive(Debug)]
 struct Game {
-    a_delta: Point2D<i32, i32>,
-    b_delta: Point2D<i32, i32>,
-    target: Point2D<i32, i32>,
+    a_delta: Point2D<isize, isize>,
+    b_delta: Point2D<isize, isize>,
+    target: Point2D<isize, isize>,
 }
 
-pub fn part_one(input: &str) -> Option<i32> {
-    let games = parse(input);
+const COSTS: (isize, isize) = (3, 1);
 
-    let total_cost = games.iter().filter_map(min_cost).sum::<i32>();
+pub fn part_one(input: &str) -> Option<isize> {
+    let games = parse(input);
+    let total_cost = games.iter().filter_map(min_cost).sum::<isize>();
 
     Some(total_cost)
 }
 
-pub fn part_two(_input: &str) -> Option<i32> {
-    None
+pub fn part_two(input: &str) -> Option<isize> {
+    const CONVERSION_DELTA: isize = 10000000000000;
+
+    let games = parse(input);
+    let total_cost = games
+        .into_iter()
+        .map(|g| Game {
+            target: point2(g.target.x + CONVERSION_DELTA, g.target.y + CONVERSION_DELTA),
+            ..g
+        })
+        .filter_map(|g| min_cost(&g))
+        .sum::<isize>();
+
+    Some(total_cost)
 }
 
-fn min_cost(game: &Game) -> Option<i32> {
-    (0..=(game.target.y / game.a_delta.y))
-        .filter_map(|a_presses| {
-            let a_total = game.a_delta.mul(a_presses);
-            let remaining_target = game.target.sub(a_total).to_point();
+fn min_cost(game: &Game) -> Option<isize> {
+    // TODO, I'm not sure this is necessarily valid for cases where a_delta and b_delta are not co-prime vectors
+    // luckily, that appears to not be the case here...
+    let b_pushes = (game.a_delta.y * game.target.x - game.a_delta.x * game.target.y)
+        / (game.a_delta.y * game.b_delta.x - game.a_delta.x * game.b_delta.y);
+    let a_pushes = (game.target.y - b_pushes * game.b_delta.y) / game.a_delta.y;
 
-            let b_presses = remaining_target.x / game.b_delta.x;
-            let b_total = game.b_delta.mul(b_presses);
+    let total = game.a_delta.mul(a_pushes) + game.b_delta.mul(b_pushes).to_vector();
 
-            let is_valid = b_total == remaining_target;
-            if is_valid {
-                Some(a_presses * 3 + b_presses)
-            } else {
-                None
-            }
-        })
-        .min()
+    if total == game.target {
+        Some(a_pushes * COSTS.0 + b_pushes * COSTS.1)
+    } else {
+        None
+    }
 }
 
 fn parse(input: &str) -> Vec<Game> {
@@ -51,14 +61,14 @@ fn parse(input: &str) -> Vec<Game> {
             let numbers = re
                 .find_iter(game)
                 .filter_map(|m| {
-                    let parsed = m.as_str().parse::<i32>();
+                    let parsed = m.as_str().parse::<isize>();
 
                     match parsed {
                         Ok(num) => Some(num),
                         _ => None,
                     }
                 })
-                .collect::<Vec<i32>>();
+                .collect::<Vec<isize>>();
 
             assert_eq!(numbers.len(), 6);
 
