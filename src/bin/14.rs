f3114a6c5e358e5c5d51bf3ba8210dbedaed4b3a
David Pugh <plastonick@gmail.com>
2024-12-15 16:52:38 +0000

    day 14 WIP

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,5 +1,6 @@
 use euclid::default::{Point2D, Vector2D};
 use euclid::{point2, vec2};
+use std::collections::{HashMap, HashSet};
 
 advent_of_code::solution!(14);
 
@@ -9,7 +10,7 @@ struct Robot {
 }
 
 impl Robot {
-    fn quadrant(self, bounds: &Point2D<isize>) -> usize {
+    fn quadrant(&self, bounds: &Point2D<isize>) -> usize {
         let central_x = self.position.x == (bounds.x - 1) / 2;
         if central_x {
             return 4;
@@ -30,32 +31,96 @@ impl Robot {
 const SECONDS: isize = 100;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let robots = parse(input);
+    let (robots, bounds) = parse(input);
 
-    // differentiate between the example and input
-    let bounds = if robots.len() == 12 {
-        point2(11, 7)
-    } else {
-        point2(101, 103)
-    };
-
-    let safety_score = robots
+    let new_robots = robots
         .iter()
         .map(|robot| move_robot(robot, SECONDS, &bounds))
-        .fold([0, 0, 0, 0, 0], |mut acc, robot| {
-            acc[robot.quadrant(&bounds)] += 1;
+        .collect();
 
-            acc
-        })
-        .iter()
-        .take(4)
-        .product();
+    let safety_score = get_quadrants(&new_robots, &bounds).iter().product();
 
     Some(safety_score)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<u32> {
+    let (robots, bounds) = parse(input);
+
+    let mut min_unique = robots
+        .iter()
+        .map(|robot| robot.position)
+        .collect::<HashSet<Point2D<isize>>>()
+        .len();
+
+    let mut i = 0;
+
+    loop {
+        i += 1;
+
+        if i > 8168 {
+            break;
+        }
+
+        let new_robots: Vec<Robot> = robots
+            .iter()
+            .map(|robot| move_robot(&robot, i, &bounds))
+            .collect();
+
+        let new_unique = new_robots
+            .iter()
+            .map(|robot| robot.position)
+            .collect::<HashSet<Point2D<isize>>>()
+            .len();
+
+        if new_unique < min_unique {
+            continue;
+        }
+
+        // manually identified...
+        min_unique = new_unique;
+
+        // println!();
+        // println!();
+        // println!();
+        // println!();
+        // println!("{i} goes");
+        // println!();
+        // println!();
+
+        // print(&new_robots, &bounds);
+    }
+
     None
+}
+
+// fn is_symmetrical(robots: &Vec<Robot>, bounds: &Point2D<isize>) -> bool {
+//     let unique_pos_robots = robots
+//         .iter()
+//         .map(|robot| (robot.position, robot))
+//         .collect::<HashMap<Point2D<isize>, Robot>>();
+//
+//     let quadrants = get_quadrants(unique_pos_robots.values().collect(), &bounds);
+//
+//     quadrants.iter().fold(HashMap::new(), |mut acc, quadrant| {
+//         *acc.entry(&quadrant).or_insert(0) += 1;
+//
+//
+//     })
+//
+//     true
+// }
+
+fn get_quadrants(robots: &Vec<Robot>, bounds: &Point2D<isize>) -> Vec<u32> {
+    robots
+        .iter()
+        .fold([0, 0, 0, 0, 0], |mut acc: [u32; 5], robot| {
+            acc[robot.quadrant(&bounds)] += 1;
+
+            acc
+        })
+        .into_iter()
+        .take(4)
+        .collect()
 }
 
 fn move_robot(robot: &Robot, seconds: isize, bounds: &Point2D<isize>) -> Robot {
@@ -68,8 +133,8 @@ fn move_robot(robot: &Robot, seconds: isize, bounds: &Point2D<isize>) -> Robot {
     }
 }
 
-fn parse(input: &str) -> Vec<Robot> {
-    input
+fn parse(input: &str) -> (Vec<Robot>, Point2D<isize>) {
+    let robots: Vec<Robot> = input
         .lines()
         .map(|l| {
             let (pos, v) = l.split_once(' ').expect("Invalid line");
@@ -79,13 +144,44 @@ fn parse(input: &str) -> Vec<Robot> {
                 velocity: numbers_to_vector(v),
             }
         })
-        .collect()
+        .collect();
+
+    // differentiate between the example and input
+    let bounds = if robots.len() == 12 {
+        point2(11, 7)
+    } else {
+        point2(101, 103)
+    };
+
+    (robots, bounds)
 }
 
 fn numbers_to_vector(input: &str) -> Vector2D<isize> {
     let (left, right) = input[2..].split_once(',').unwrap();
 
     vec2(left.parse().unwrap(), right.parse().unwrap())
+}
+
+fn print(robots: &Vec<Robot>, bounds: &Point2D<isize>) {
+    let positions = robots.iter().fold(HashMap::new(), |mut acc, robot| {
+        let key = (robot.position.x, robot.position.y);
+
+        *acc.entry(key).or_insert(0) += 1;
+        acc
+    });
+
+    for x in 0..bounds.x {
+        for y in 0..bounds.y {
+            let tile = match positions.get(&(x, y)) {
+                Some(tile) => tile.to_string().chars().collect::<Vec<char>>()[0],
+                None => ' ',
+            };
+
+            print!("{}", tile);
+        }
+
+        println!();
+    }
 }
 
 #[cfg(test)]
