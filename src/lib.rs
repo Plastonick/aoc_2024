5fee6bbaeb059e75eb61bf0eeec6ee0d8a0ca374
David Pugh <plastonick@gmail.com>
2024-12-25 14:37:35 +0000

    transpose function... lasted until the last day without using!

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
 pub mod template;
 
-// Use this file to add helper functions and additional modules.
+pub fn transpose2<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    assert!(!v.is_empty());
+
+    let len = v[0].len();
+    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .map(|n| n.next().unwrap())
+                .collect::<Vec<T>>()
+        })
+        .collect()
+}
