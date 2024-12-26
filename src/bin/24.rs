49d60a5577d788fbe8949b4495a9ef2684e94141
David Pugh <plastonick@gmail.com>
2024-12-26 12:40:03 +0000

    this doesn't feel right...

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -5,130 +5,228 @@ use std::ops::{BitAnd, BitOr, BitXor};
 
 advent_of_code::solution!(24);
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 enum Operation {
     And,
     Or,
     Xor,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Process {
-    input1: Box<Node>,
-    input2: Box<Node>,
+    input1: usize,
+    input2: usize,
     operation: Operation,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 enum Value {
     Derived(Process),
     Raw(usize),
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Node {
     address: usize,
     value: Value,
 }
 
 impl Node {
-    fn derive_value(&self) -> usize {
-        match &self.value {
-            Raw(value) => *value,
-            Derived(process) => match process.operation {
-                Operation::And => process
-                    .input1
-                    .derive_value()
-                    .bitand(process.input2.derive_value()),
-                Operation::Or => process
-                    .input1
-                    .derive_value()
-                    .bitor(process.input2.derive_value()),
-                Operation::Xor => process
-                    .input1
-                    .derive_value()
-                    .bitxor(process.input2.derive_value()),
-            },
+    fn derive_value(&self, nodes: &Vec<Node>, stack: usize) -> Option<usize> {
+        if stack == 0 {
+            None
+        } else {
+            Some(match &self.value {
+                Raw(value) => *value,
+                Derived(process) => match process.operation {
+                    Operation::And => nodes[process.input1]
+                        .derive_value(&nodes, stack - 1)?
+                        .bitand(nodes[process.input2].derive_value(&nodes, stack - 1)?),
+                    Operation::Or => nodes[process.input1]
+                        .derive_value(&nodes, stack - 1)?
+                        .bitor(nodes[process.input2].derive_value(&nodes, stack - 1)?),
+                    Operation::Xor => nodes[process.input1]
+                        .derive_value(&nodes, stack - 1)?
+                        .bitxor(nodes[process.input2].derive_value(&nodes, stack - 1)?),
+                },
+            })
+        }
+    }
+
+    fn get_chain(self, nodes: &Vec<Node>) -> Vec<Node> {
+        match self.value {
+            Derived(process) => vec![
+                nodes[process.input1].clone().get_chain(&nodes),
+                nodes[process.input2].clone().get_chain(&nodes),
+            ]
+            .concat(),
+            Raw(_) => {
+                vec![self]
+            }
         }
     }
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let (nodes, address_map) = parse(input);
+    let (nodes, _, address_map) = parse(input);
 
     Some(collated_values('z', &nodes, &address_map))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let (nodes, address_map) = parse(input);
+    let (mut nodes, addresses, address_map) = parse(input);
     let x = collated_values('x', &nodes, &address_map);
     let y = collated_values('y', &nodes, &address_map);
-    let z = collated_values('z', &nodes, &address_map);
     let expected_sum = x + y;
-    let actual_sum = z;
 
-    println!(
-        "Expected {} but got {} for a diff of {}",
-        expected_sum,
-        actual_sum,
-        to_bin(expected_sum.abs_diff(actual_sum))
-    );
+    // println!(
+    //     "Expected {} but got {} with bitwise diffs {}",
+    //     expected_sum,
+    //     actual_sum,
+    //     to_bin(expected_sum.bitxor(actual_sum))
+    // );
 
-    // let composed_values = composed_values(values.clone(), processes.clone());
-    //
-    // dbg!(composed_values
-    //     .into_iter()
-    //     .filter(|(k, _)| k.starts_with('z'))
-    //     .map(|(k, v)| format!("{k} => {v}"))
-    //     .sorted()
-    //     .collect::<Vec<String>>());
-    //
-    // let values = run_program(values, processes);
-    // let z = calc_value(&values, 'z');
-    //
-    // let expected = x + y;
-    // let diff = expected.bitxor(z);
-    //
-    // println!("{x} + {y} = {z}");
-    // println!("Gives us a diff of {}", to_bin(diff));
+    loop {
+        let improvement = fix_nodes(nodes, &address_map, &addresses, expected_sum);
 
-    // let values = run_program(values, processes.clone());
-    // let expected = x + y;
+        if improvement.is_none() {
+            break;
+        }
+
+        println!("found an improvement, looping");
+        nodes = improvement.unwrap();
+    }
+
+    // while let Some(new_nodes) = fix_nodes(nodes, address_map, addresses, expected_sum) {
+    //     nodes = new_nodes;
+    //     println!("fixing some more nodes!");
     //
-    // let mut processes = processes;
-    // while let Some((p1, p2, improvement)) = find_best_switch(&values, &processes, expected) {
-    //     println!(
-    //         "Switching {} and {} for an improvement of {}",
-    //         p1, p2, improvement
-    //     );
-    //     let p2_buffer = processes.remove(p2);
-    //     let p1_buffer = processes.remove(p1);
-    //
-    //     let p2_target = p2_buffer.target.clone();
-    //     let p1_target = p1_buffer.target.clone();
-    //
-    //     let process1 = Process {
-    //         target: p2_target,
-    //         ..p1_buffer
-    //     };
-    //     let process2 = Process {
-    //         target: p1_target,
-    //         ..p2_buffer
-    //     };
-    //
-    //     processes.push(process2);
-    //     processes.push(process1);
+    //     panic!("Done");
     // }
+
+    // previously: 110000110011110000000000000000111000000
+    // now: TODO
+
+    // let bin_diff = to_bin(expected_sum.bitxor(actual_sum));
+    //
+    // let i = *address_map.get("kpb").unwrap();
+    // let chain = nodes[i].clone().get_chain();
+    // let chain_str = chain
+    //     .iter()
+    //     .map(|x| addresses[x.address].clone())
+    //     .join(", ");
+    //
+    // dbg!(addresses[i].clone(), chain_str);
 
     None
 }
 
-fn collated_values(prefix: char, nodes: &Vec<Node>, address_map: &HashMap<String, usize>) -> usize {
+fn fix_nodes(
+    nodes: Vec<Node>,
+    address_map: &HashMap<String, usize>,
+    addresses: &Vec<String>,
+    expected: usize,
+) -> Option<Vec<Node>> {
+    let actual_sum = collated_values('z', &nodes, &address_map);
+    println!("exp: {}\nact: {}", to_bin(actual_sum), to_bin(expected));
+
+    let bit_diffs = to_bin(actual_sum.bitxor(expected));
+
+    // fix the smallest wrong bit first
+    let index =
+        bit_diffs
+            .chars()
+            .rev()
+            .enumerate()
+            .find_map(|(i, bit)| if bit == '1' { Some(i) } else { None })?;
+
+    // we have a smallest wrong bit... so something in the node-chain for this one is wrong, but the node-chain
+    // below it _isn't_ wrong.
+    // find the difference!
+
+    println!(
+        "smallest wrong bit {} shouldn't be {} in bit_xor {}",
+        index,
+        bit_diffs.get(index..index + 1).unwrap(),
+        bit_diffs
+    );
+
+    let node_address_str = format!("z{:0>2}", index);
+    println!("bad address: {}", node_address_str);
+
+    let broken_node_address = address_map.get(&node_address_str).unwrap();
+    let broken_node = nodes[*broken_node_address].clone();
+
+    for a_node in broken_node.get_chain(&nodes).iter() {
+        for b_node in nodes.iter() {
+            if a_node.address == b_node.address {
+                continue;
+            }
+
+            let new_a = Node {
+                address: a_node.address,
+                value: b_node.value.clone(),
+            };
+            let new_b = Node {
+                address: b_node.address,
+                value: a_node.value.clone(),
+            };
+
+            // try a swap?
+            let mut new_nodes = nodes.clone();
+            new_nodes[a_node.address] = new_a;
+            new_nodes[b_node.address] = new_b;
+
+            let new_value = collated_values('z', &new_nodes, &address_map);
+            println!("new: {}\nact: {}", to_bin(new_value), to_bin(expected));
+
+            let bit_diffs = to_bin(actual_sum.bitxor(expected));
+            let new_least_index = bit_diffs.chars().rev().enumerate().find_map(|(i, bit)| {
+                if bit == '1' {
+                    Some(i)
+                } else {
+                    None
+                }
+            });
+
+            let Some(new_least_index) = new_least_index else {
+                // hurrah! We've totally fixed it. Return our new nodes!
+                return Some(new_nodes);
+            };
+
+            // we've not fixed it, but have we improved it?
+            if new_least_index > index {
+                // it's better! Consider it an improvement and move along...
+
+                println!(
+                    "Improved by switching {} and {}",
+                    addresses[a_node.address], addresses[b_node.address]
+                );
+                panic!();
+                return Some(new_nodes);
+            } else {
+                println!(
+                    "it's worse! {} instead of {} Try again...",
+                    new_least_index, index
+                );
+            }
+        }
+    }
+
+    // TODO
+    Some(vec![])
+}
+
+fn collated_values(
+    prefix: char,
+    nodes: &Vec<Node>,
+    address_map: &HashMap<String, usize>,
+) -> Option<usize> {
     address_map
         .iter()
         .filter(|(k, _)| k.starts_with(prefix))
         .sorted_by(|(a, _), (b, _)| a.cmp(b))
-        .map(|(_, &address)| nodes[address].derive_value())
+        .map(|(_, &address)| nodes[address].derive_value(&nodes, 100))
         .enumerate()
         .map(|(ord, value)| value * 2_usize.pow(ord as u32))
         .sum()
@@ -222,22 +320,10 @@ fn to_bin(int: usize) -> String {
     binary
 }
 
-fn calc_value(values: &HashMap<String, usize>, prefix: char) -> usize {
-    values
-        .keys()
-        .sorted()
-        .filter(|k| k.starts_with(prefix))
-        .enumerate()
-        .map(|(ord, k)| {
-            let bin_digit = values.get(k).unwrap();
-            bin_digit * 2_usize.pow(ord as u32)
-        })
-        .sum::<usize>()
-}
-
-fn parse(input: &str) -> (Vec<Node>, HashMap<String, usize>) {
+fn parse(input: &str) -> (Vec<Node>, Vec<String>, HashMap<String, usize>) {
     let mut address_map: HashMap<String, usize> = HashMap::new();
-    let mut nodes = Vec::with_capacity(input.lines().count());
+    let mut addresses = Vec::new();
+    let mut nodes = Vec::new();
     let mut lines = input
         .lines()
         .filter(|l| !l.is_empty())
@@ -248,6 +334,7 @@ fn parse(input: &str) -> (Vec<Node>, HashMap<String, usize>) {
             let (address_str, value) = line.split_once(": ").unwrap();
             let address = nodes.len();
             address_map.insert(address_str.to_owned(), address);
+            addresses.push(address_str.to_owned());
 
             nodes.push(Node {
                 address,
@@ -267,14 +354,18 @@ fn parse(input: &str) -> (Vec<Node>, HashMap<String, usize>) {
                 continue;
             };
 
+            // everyone loves a consistent ordering!
+            let (address1, address2) = (address1.min(address2), address1.max(address2));
+
             let address = nodes.len();
             address_map.insert(parts[4].to_owned(), address);
+            addresses.push(parts[4].to_owned());
 
             nodes.push(Node {
                 address,
                 value: Derived(Process {
-                    input1: Box::from(nodes[address1].clone()),
-                    input2: Box::from(nodes[address2].clone()),
+                    input1: address1,
+                    input2: address2,
                     operation: match parts[1] {
                         "AND" => Operation::And,
                         "OR" => Operation::Or,
@@ -288,7 +379,7 @@ fn parse(input: &str) -> (Vec<Node>, HashMap<String, usize>) {
         }
     }
 
-    (nodes, address_map)
+    (nodes, addresses, address_map)
 }
 
 #[cfg(test)]
@@ -312,7 +403,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
         assert_eq!(result, Some(1));
     }
 }
